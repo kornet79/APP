25361cd0ba9e015fcc7197265be91821a28fb620
Pete <kornet79@naver.com>
2024-09-23 17:22:21 +0900

    commit

---
+++ my-first-Pete/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>나의 첫번째 앱을 만들 쁘띠 프로젝트</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
