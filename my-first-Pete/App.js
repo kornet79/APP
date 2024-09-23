5134ce0b3e50c9332888cfb146757b24a14c809d
Pete <kornet79@naver.com>
2024-09-23 17:44:08 +0900

    commit

--- my-first-Pete/App.js
+++ my-first-Pete/App.js
@@ -2,9 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('Expo React Native');
+
+  const name = 'Gyumin';
+  const isFullname = true;
+
+  const add = (a, b) => {
+    return a + b;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>나의 첫번째 앱을 만들 쁘띠 프로젝트</Text>
+      <Text>My name is {name}</Text>
+      <Text>a + 2 = {add(1, 2)}</Text>
+      <Text>{isFullname ? name + ' kim' : name}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
   },
 });
