d0304306399abfa3fddaafe2d4a34b3b8515035a
Pete <kornet79@naver.com>
2024-09-23 18:16:20 +0900

    commit

--- my-first-Pete/src/App.js
+++ my-first-Pete/src/App.js
@@ -13,8 +13,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#fff',
   },
 });
